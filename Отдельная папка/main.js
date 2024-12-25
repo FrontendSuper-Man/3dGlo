a9a31e8d04c04d2e05d234f49aae39c2f9baeb31
Худойкулов Суннат <khudoykulov07@bk.ru>
2024-12-26 01:04:47 +0500

    First commit

--- Отдельная папка/main.js
+++ Отдельная папка/main.js
@@ -1,27 +1,47 @@
 let readyAlert
 
-const dateNow = new Date().getTime()
-if (condition) {
+const getTimeUntilNewYear = () => {
+	const currentDate = new Date();
+	const newYear = new Date(currentDate.getFullYear() + 1, 0, 1);
+	const timeRemaining = newYear - currentDate;
+	const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+	return { days };
+};
 
+const timeUntilNewYear = getTimeUntilNewYear();
+
+let hours = new Date().getHours()
+let hourMes
+if (hours <= 6 && hours > 0) {
+	hourMes = 'Доброй ночи';
+} else if (hours <= 12 && hours > 6) {
+	hourMes = 'Доброе утро';
+} else if (hours < 18 && hours > 12) {
+	hourMes = 'Добрый день';
 } else {
-
+	hourMes = 'Доброго вечера';
 }
 
-let newYearDay
+const minutes = new Date().getMinutes()
+const seconds = new Date().getSeconds()
+
+const weekDay = new Date().getDay()
+let weekMes
+weekDay === 1 ? weekMes = "Понедельник" : null
+weekDay === 2 ? weekMes = "Вторник" : null
+weekDay === 3 ? weekMes = "Среда" : null
+weekDay === 4 ? weekMes = "Четверг" : null
+weekDay === 5 ? weekMes = "Пятница" : null
+weekDay === 6 ? weekMes = "Суббота" : null
+weekDay === 7 ? weekMes = "Воскресенье" : null
 
 
-let hours = Math.floor((timeRemaining / 60 / 60) % 24)
 let hourMess
-if (hours >= 0 && hours <= 12) {
-	hourMess = 'AM'
-} else {
-	hourMess = 'PM'
-}
-let minutes = Math.floor((timeRemaining / 60) % 60)
-let seconds = Math.floor(timeRemaining % 60)
+hours >= 0 && hours <= 12 ? hourMess = 'AM' : hourMess = 'PM'
 
 readyAlert = `
+${hourMes}
+Сегодня: ${weekMes}
 Текущее время ${hours}:${minutes}:${seconds} ${hourMess} 
-До нового года осталось + ${newYearDay}`
-
+До нового года осталось ${timeUntilNewYear.days} Дней`
 alert(readyAlert)
