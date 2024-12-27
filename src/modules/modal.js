4b7e70808675fa9f80865f123aed02c065a7ac75
Худойкулов Суннат <khudoykulov07@bk.ru>
2024-12-27 19:53:53 +0500

    Сделал дз

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,15 +1,42 @@
 const modal = () => {
-	const buttons = document.querySelectorAll('.popup-btn')
-	const modal = document.querySelector('.popup')
-	const closeBtn = modal.querySelector('.popup-close')
-	console.log(modal);
-	closeBtn.addEventListener('click', () => {
-		modal.style.display = 'none'
-	})
+	const buttons = document.querySelectorAll('.popup-btn');
+	const modal = document.querySelector('.popup');
+	const closeBtnPop = modal.querySelector('.popup-close');
+
+	const closeModal = () => {
+		if (window.innerWidth >= 768) {
+			modal.style.transform = 'scale(0)';
+			modal.style.opacity = '0';
+			setTimeout(() => {
+				modal.style.display = 'none';
+			}, 300);
+		} else {
+			modal.style.display = 'none';
+		}
+	};
+
+	const openModal = () => {
+		modal.style.display = 'block';
+
+		if (window.innerWidth >= 768) {
+			setTimeout(() => {
+				modal.style.transform = 'scale(1)';
+				modal.style.opacity = '1';
+			}, 10);
+		} else {
+			modal.style.transform = 'none';
+			modal.style.opacity = '1';
+		}
+	};
+
+	closeBtnPop.addEventListener('click', closeModal);
 	buttons.forEach(btn => {
-		btn.addEventListener('click', () => {
-			modal.style.display = 'block'
-		})
-	})
-}
-export default modal
+		btn.addEventListener('click', openModal);
+	});
+
+	modal.addEventListener('click', (event) => {
+		event.target === modal ? closeModal() : null
+	});
+};
+
+export default modal;
