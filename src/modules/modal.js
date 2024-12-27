5b666792e4584f27aedb2bf40917577e7a8d1771
Худойкулов Суннат <khudoykulov07@bk.ru>
2024-12-27 19:17:23 +0500

    Сделал модальное окно

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,5 +1,15 @@
 const modal = () => {
-	
+	const buttons = document.querySelectorAll('.popup-btn')
+	const modal = document.querySelector('.popup')
+	const closeBtn = modal.querySelector('.popup-close')
+	console.log(modal);
+	closeBtn.addEventListener('click', () => {
+		modal.style.display = 'none'
+	})
+	buttons.forEach(btn => {
+		btn.addEventListener('click', () => {
+			modal.style.display = 'block'
+		})
+	})
 }
-
 export default modal
