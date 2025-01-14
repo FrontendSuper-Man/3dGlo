940673f764856401bf877fc11057e3a64a1c8d09
Sunnat Khudoykulov <khudoykulov07@bk.ru>
2025-01-14 21:54:23 +0500

    Сделал отправку данных

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import modal from './modules/modal';
 import calc from './modules/calc';
 import tabs from './modules/tabs';
 import portfolio from './modules/portfolio';
+import sendForm from './modules/sendForm';
 
 
 dots()
@@ -14,3 +15,12 @@ modal()
 calc(100)
 tabs()
 portfolio()
+sendForm({
+	formId: 'form123',
+	someElem: [
+		{
+			type: 'block',
+			id: "total"
+		}
+	],
+})
